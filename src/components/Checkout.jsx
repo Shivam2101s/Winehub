ece1f744c6e688dbe10e91ada6ea368d400479fd
Shivam Sharma <mechbuddy.shivam@gmail.com>
2022-03-08 23:36:08 +0530

    done

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -3,18 +3,9 @@ import { useNavigate } from "react-router-dom";
 import "./Checkout.css";
 
 export const Checkout = () => {
-  const [total, setTotal] = useState(0);
   const [form, setForm] = useState([]);
   const navigate = useNavigate();
 
-  useEffect(() => {
-    getData();
-  }, []);
-
-  const getData = () => {
-    let res = JSON.parse(localStorage.getItem("cart_total"));
-    setTotal(res);
-  };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -24,25 +15,26 @@ export const Checkout = () => {
       [name]: value,
     });
   };
-  
-  const handleClick = () => {
-      if(form.nam ===undefined  || form.time ===undefined  || form.contact ===undefined  || form.vehicle ===undefined ){
-             alert("Please fill all the details !!")
-      }else{ 
-     localStorage.setItem("punctured_booking",JSON.stringify(form));
-      alert("Appointment booked successfully !!")
-      navigate("/success");
-         
-      }
-   
-  };
-  console.log("Booking : ",form)
 
+  const handleClick = () => {
+    if (
+      form.nam === undefined ||
+      form.email === undefined ||
+      form.contact === undefined ||
+      form.address === undefined 
+    ) {
+      alert("Please fill all the details !!");
+    } else {
+      alert("Order Placed successfully !!");
+      navigate("/");
+    }
+  };
+  
   return (
     <div id="checkDiv">
       <div>
         <h1 id="check_head">Checkout</h1>
-        <p id="check_sec_head">Your vehicle, Our responsibility</p>
+        <p id="check_sec_head">The Royal Taste</p>
       </div>
 
       <div id="customer_details">
@@ -60,22 +52,19 @@ export const Checkout = () => {
           placeholder="Contact No."
           onChange={handleChange}
         />
-        <label htmlFor="time">Time</label>
-        <input type="time" name="time" onChange={handleChange} />
-        <label htmlFor="vehicle">Vehicle</label>
+        <label htmlFor="email">Email</label>
+        <input type="email" name="email" placeholder="Email" onChange={handleChange} />
+        <label htmlFor="address">Address</label>
         <input
           type="text"
-          name="vehicle"
-          placeholder="Vehicle Type"
+          name="address"
+          placeholder="Address"
           onChange={handleChange}
         />
       </div>
       <div id="check_bottom">
-        <p id="check_total">
-          <b>Total :</b> ₹ {total} including GST and services charges.
-        </p>
         <button id="check_btn" onClick={handleClick}>
-          Book Appointment
+          Place Order 
         </button>
       </div>
     </div>
